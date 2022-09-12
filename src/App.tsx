93e8cbae7f266b65f67aaa6f19f3d9bfa1726275
MTomys <michalltomys@protonmail.ch>
2022-09-12 15:58:21 +0200

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="bg-slate-700">
+      <h1>Hello,world!</h1>
+    </div>
+  );
+}
+
+export default App;
