4ed6b3e4503348088718d39ede963c8e79cd42fc
MTomys <michalltomys@protonmail.ch>
2022-09-13 20:31:18 +0200

    cleanup

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,54 +2,44 @@ import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 import { stringify } from 'querystring';
 import { useState } from 'react';
 import { mapContainerStyle, mapCenter } from '../utils/MapConfig';
+import { CoordinateWithOccurences } from '../utils/MapHelpers';
 import * as mapHelpers from '../utils/MapHelpers';
 
-interface Coordinate {
-  lat: number;
-  lng: number;
-}
+// interface Coordinate {
+//   lat: number;
+//   lng: number;
+// }
 
-interface CoordinateWithOccurences {
-  coord: Coordinate;
-  occurences: number;
-}
+// interface CoordinateWithOccurences {
+//   coord: Coordinate;
+//   occurences: number;
+// }
 
-const randomLatMax = 51.95;
-const randomLatMin = 51.95;
+// const randomLatMax = 51.95;
+// const randomLatMin = 51.95;
 
-const randomLngMax = 20.42;
-const randomLngMin = 20.42;
+// const randomLngMax = 20.42;
+// const randomLngMin = 20.42;
 
-const generateRandomLatitude = () => {
-  return Math.random() * (randomLatMax - randomLatMin + 1) + randomLatMin;
-};
+// const generateRandomLatitude = () => {
+//   return Math.random() * (randomLatMax - randomLatMin + 1) + randomLatMin;
+// };
 
-const generateRandomLongitude = () => {
-  return Math.random() * (randomLngMax - randomLngMin + 1) + randomLngMin;
-};
+// const generateRandomLongitude = () => {
+//   return Math.random() * (randomLngMax - randomLngMin + 1) + randomLngMin;
+// };
 
-const generateRandomCoordinates = () => {
-  const coordinates: Coordinate[] = [];
-  for (let i = 0; i < 100; i++) {
-    let latitude = generateRandomLatitude();
-    let longitude = generateRandomLongitude();
-    coordinates.push({ lat: latitude, lng: longitude });
-  }
-  return coordinates;
-};
+// const generateRandomCoordinates = () => {
+//   const coordinates: Coordinate[] = [];
+//   for (let i = 0; i < 100; i++) {
+//     let latitude = generateRandomLatitude();
+//     let longitude = generateRandomLongitude();
+//     coordinates.push({ lat: latitude, lng: longitude });
+//   }
+//   return coordinates;
+// };
 
 export const Map: React.FC = () => {
-  const [coordinates, useCoordinates] = useState<Coordinate[]>(
-    generateRandomCoordinates()
-  );
-
-  const [equalCoordinates, useEqualCoordinates] = useState<Coordinate[]>([
-    { lat: 51.95, lng: 20.42 },
-    { lat: 51.95, lng: 20.42 },
-    { lat: 52, lng: 20.45 },
-    { lat: 52, lng: 20.45 },
-  ]);
-
   const [coordinatesMap, useCoordinatesMap] = useState<
     CoordinateWithOccurences[]
   >(mapHelpers.parseCoordData(mapHelpers.data));
